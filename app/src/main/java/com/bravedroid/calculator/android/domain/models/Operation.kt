3a81723e93d1e3a17a4d7fa3d6d2853fbf8f6240
refka-farid <refka.farid@gmail.com>
2023-01-20 18:30:55 +0100

    [calc] Add ExpressionParser

---
+++ app/src/main/java/com/bravedroid/calculator/android/domain/models/Operation.kt
@@ -0,0 +1,18 @@
+package com.bravedroid.calculator.android.domain.models
+
+enum class Operation(val symbol: Char) {
+    ADD('+'),
+    SUBTRACT('-'),
+    MULTIPLY('x'),
+    DIVIDE('/'),
+    PERCENT('%'),
+    ;
+
+    companion object {
+        fun fromSymbol(symbol: Char): Operation =
+            values().find { it.symbol == symbol }
+                ?: throw IllegalArgumentException("Invalid symbol")
+
+        val OperationSymbols = Operation.values().map { it.symbol }.joinToString("")
+    }
+}
