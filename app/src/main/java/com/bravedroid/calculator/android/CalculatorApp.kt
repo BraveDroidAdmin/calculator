0b9197623a65ff96725d1124a0e7abf4b37e1d04
houssemzaier <houssem.zaier@gmail.com>
2023-01-24 22:31:32 +0100

    [calc]setup Crashlytics

---
+++ app/src/main/java/com/bravedroid/calculator/android/CalculatorApp.kt
@@ -0,0 +1,9 @@
+package com.bravedroid.calculator.android
+
+import android.app.Application
+
+class CalculatorApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
